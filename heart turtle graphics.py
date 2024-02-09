00b30d3f09e98c5a0178d139d6bcecd882f496f7
PRIYAtechky <158708326+PRIYAtechky@users.noreply.github.com>
2024-02-09 22:17:41 +0530

    Add files via upload

---
+++ heart turtle graphics.py
@@ -0,0 +1,14 @@
+from turtle import*
+bgcolor("pink")
+color("white")
+begin_fill()
+pensize(10)
+left(50)
+forward(133)
+circle(50,200)
+right(140)
+circle(50,200)
+forward(133)
+end_fill()
+        
+    
