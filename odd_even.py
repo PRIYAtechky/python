15781e818a11a20d570f92bef760e52edf23bbc8
PADMAPRIYA <158708326+PRIYAtechky@users.noreply.github.com>
2026-01-25 21:26:33 +0530

    Update odd_even.py

--- odd_even.py
+++ odd_even.py
@@ -61,3 +61,53 @@ else:
 
 
 
+for i in "apple":
+    print(i)
+
+
+
+
+for i in range(5):
+    print(i)
+
+
+
+
+for i in range(1,5):
+    print(i)
+
+
+
+
+for i in range(1,11):
+    print(i,"X 2 =",i*2)
+
+
+a=int(input("A:"))
+b=int(input("B:"))
+for i in range(a+1,b):
+    print(i)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
