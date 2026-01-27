f8a5020fc458e03242524048a12ee22b465f1ee9
PADMAPRIYA <158708326+PRIYAtechky@users.noreply.github.com>
2026-01-27 21:27:03 +0530

    Implement simple calculator with basic operations

---
+++ simple_calculator.py
@@ -0,0 +1,14 @@
+a=int(input("Value for A: "))
+b=int(input("Value for B: "))
+operation=input("choose add/sub/mul/div: ")
+if(operation=="add"):
+    print(a+b)
+elif(operation=="sub"):
+    print(a-b)
+elif(operation=="mul"):
+    print(a*b)
+elif(operation=="div"):
+    print(a/b)
+else:
+    print("Invalid Operation")
+  
